ebfc365401b8392ab1fa20a21e994acfde9ff020
Rita Nwoye <cheremoye18@gmail.com>
2026-03-17 09:54:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,329 @@
+// // //  let todayIsTuesday = true;
+// // //     console.log(todayIsTuesday)
+// // //     let isAdult = false;
+// // //     if (isAdult) {
+// // //         console.log("access granted")
+// // //     } else {
+// // //         console.log("access denied")
+// // //     }
+// // //     let fruits = ["mango", "kiwi", "apple"];
+// // //     let names = ["Fortune", "Kendra", "Victor"];
+// // //     let cars = ["Honda", "Toyota", "camry"];
+// // //     console.log(fruits,names,cars);
+// // //    let student0 = {
+// // //      name: "Samuel",
+// // //        age: 15,
+// // //         score: 80, }
+
+// // //           let student1 = {
+// // //      name: "Ella",
+// // //        age: 15,
+// // //         score: 80, } 
+        
+// // //         let student3 = {
+// // //      name: "Josh",
+// // //        age: 15,
+// // //         score: 80, }
+
+// // //         let students = [student0, student1, student3];
+// // //         console.log(students)
+// // //         console.log('second student',students[1]);
+
+// // //     //student.score = 50
+// // //     //student.name = "Ada"
+// // //    // student.age = 19 
+// // //    // student.hobby = "football"
+// // //    // delete student.age;
+// // //     //console.log('student',student);
+// // //     //console.log(student.name);
+// // //     //console.log(student.age);
+// // //     //console.log(student["age"]);
+// // //     //console.log(student["score"]);
+// // //    // console.log(student.hobby)
+
+// // // //    let students = {
+// // // //     student[0]
+// // // //     name: "Ella",
+// // // //     age: 10,
+// // // //     score: 30,
+     
+// // // //    student[1]
+// // // //    name:"Joshua",
+// // // //    age: 13,
+// // // //    score: 40,
+
+// // // //    student[2]
+// // // //    name: "hannah"
+
+
+// // // //    }
+
+    
+// // //      //let name = "Rita";
+// // //     //console.log(name)
+// // //    let _age = 9;
+// // //    console.log(_age)
+
+// // let city = "Anambra";
+// // console.log(city)
+
+// // let isOnline = false;
+// // if (isOnline) {
+// //    console.log("access granted");
+// // } else {
+// //     console.log("access denied");
+// // }
+
+// // let firstName = "Rita";
+// // let lastName = "Nwoye";
+// // let fullName = firstName + " " + lastName
+// // console.log(fullName);
+// // console.log("Hello" + " "  + fullName);
+
+// // // let age = "15"
+// // // console.log("I'm" + " " + age + " " + "years old")
+
+// // let obj1 = {
+// //     name: "Tunde",
+// // } 
+// // let obj2 = obj1
+// // console.log(obj2)
+// // obj2.name = "Mike"
+// // console.log(obj1.name)
+// // console.log(obj2.name)
+
+// // let result = 10 + 5
+// // console.log(result)
+
+// // let answer = 10 % 5
+// // console.log(answer) 
+// // let output = 7 % 3
+// // console.log(output)
+// // console.log(5>2)
+// // console.log(2>5)
+// // console.log(5 == 5)
+// // console.log (5 === "3")
+// // console.log(5 == "5")
+// // console.log(5 != 3)
+
+// // // let  score = 50
+// // // score += 10
+// // // console.log(score)
+
+// // let score = 50
+// // score += 10;
+// // score += 20;
+// // score += 30;
+// // score += 15;
+// // console.log(score)
+
+// // score -= 20;
+// // console.log(score)
+
+// // score *= 10
+// // console.log(score)
+// // score /= 2
+// // console.log(score)
+
+// // // product operators
+// // let price = 5000;
+// // let quantity = 10;
+// // let product = price * quantity;
+// // console.log(product);
+// // let division = product / price
+// // console.log(division)
+
+// // //logical operators
+// // //AND operator
+// // let age1 = 18;
+// // let hasID = false
+// // if ( age1 >= 18 && hasID) {
+// //     console.log("access granted")
+// // } else {
+// //     console.log("access denied")
+// // };
+
+// // // OR operator
+// // let Day = "Saturday";
+// // if (Day === "Saturday" || Day === "sunday") {
+// //     console.log("it's weekend")
+// // } 
+
+// // //NOT operator
+// // let isLoggedIn = false;
+// // if (!isLoggedIn) {
+// //     console.log("logged in")
+// // }
+
+// // //multiple logical operators example 1
+// // // let ageNumber = 22;
+// // // let hasTicket = false;
+// // // let isVip = true
+// // // let isBanned = false
+// // // if (ageNumber > 18 && ! hasTicket) {
+// // //     console.log("entry denied");
+// // // } else {
+// // //     console.log("entry granted");
+
+// // //  if ((ageNumber > 18 && hasTicket) || isVip) {
+// // // if (! isBanned) {
+// // //     console.log("access granted");
+// // // } else {
+// // //     console.log("access denied banned user");
+// // // } 
+// // // }
+// // // else {
+// // //     console.log("access denied");
+// // // };
+
+// // //example 2
+// // let isSignedIn = true;
+// // let isPremiumUser = false;
+// // let hasDownLoadCode = true;
+// // let isSuspended = false
+// // if ((isSignedIn && isPremiumUser) || hasDownLoadCode) {
+// //  if (isSuspended) {
+// //      console.log("user cannot download : suspended");
+// //  } else {
+// //      console.log("user can download");
+// //  }  
+
+// // }
+// //   else {
+// //     console.log("user cannot download")
+// //   }
+
+// //   // Assignment
+// //   let name = " Jordan";
+// //   let age = 20;
+// //   let isMember = true;
+// //   let hasCoupon = false;
+// //   let isBanned = false;
+// //   let ticketPrice = 60;
+
+// //   if (isBanned) {
+// //     console.log(name + " " + "is not allowed to enter the movie");
+// //   } if (age >= 18) {
+// //     console.log(name + " " + "is allowed to enter the movie");
+// //   } else {
+// //     console.log(name + " " + "is not allowed to enter the movie");
+// //   } if ((isMember || hasCoupon) && ticketPrice > 50) {
+// //     console.log("Discount applied");
+// //   } if ((!isMember) || (!hasCoupon)) {
+// //     console.log(ticketPrice = 45);
+// //   };
+
+
+// // if/else if/ else statement 13/03/2026
+// // let scoreNumber = 75;
+// // if (scoreNumber >= 70) {
+// //     console.log("score grade A");
+// // } else if (scoreNumber >= 60) {
+// //     console.log("score grade B");
+// // } else if (scoreNumber >= 50) {
+// //     console.log("score grade C");
+// // } else {
+// //     console.log("failed");
+// // };
+
+// // //classwork
+// // let age = 16;
+// // if (age >=18) {
+// //     console.log(" An adult");
+// // } else if (age >= 13) {
+// //     console.log("A teenager");
+// // } else {
+// //     console.log("A child");
+// // }
+
+// // //Loop
+// // // console.log(1);
+// // // console.log(2);
+// // // console.log(3);
+// // // console.log(4);
+// // // console.log(5);
+
+// // // How to write a for loop
+// // for (let i = 3; i <= 10; i+=2) {
+// // console.log(i)
+// // }
+
+// // //another example
+// // let fruits = ["Mango", "Guava", "Kiwi","Apple"]
+// // fruits[1]="Plum"
+// // for (let i = 0; i < fruits.length; i++) {
+
+// //     console.log(fruits[i]);
+// // }
+// // // console.log(fruits[0])
+// // // console.log(fruits[1])
+// // // console.log(fruits[2])
+
+// // //while loop
+// let i = 0;
+// while(i<=5) {
+//     console.log(i)
+//     i++
+// }
+
+
+// for (let i = 100; i >= 1; i--) {
+// if (i%4==0) {
+//     console.log(i)
+// }
+// }
+
+// //example
+// let word = "Remain";
+// for (let i = 0; i < word.length; i++) {
+//     console.log(word[i])
+// }
+
+// // another example using sum
+// let sum = 0
+// for (let i = 1; i <= 20; i++) {
+//  sum += i 
+//  console.log(sum)
+// }
+
+// Assignment on loop
+// Question 1
+let sum = 0
+for (let i= 1;i <= 30; i++)
+    if (i % 3 == 0) {
+        sum += i
+    }
+    console.log(sum)
+
+//Question 2
+let count1 = 0
+for (let i = 1; i <= 50; i++)
+    if (i % 2 == 0) {
+       count1++ 
+    }
+console.log(count1)
+
+//Question 3
+let count = 20
+for (let i = 20; i >=1; i--)
+    if (i % 4 !== 0) {
+     console.log(i)   
+    }
+
+  //Question 5
+  let arr = [4, 10, 2,8];
+  console.log(arr[1]);
+
+//Question 6
+let fruits = ["apple", "banana", "orange", "mango"];
+for (let i = 0; i < fruits.length; i++) {
+console.log(fruits[i])
+}
+
+
+
+
+
+
+    
+
